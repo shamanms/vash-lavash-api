5899373d0c3e980457c557648e4585b379b02c38
PochUa <po4a123@gmail.com>
2022-09-19 22:40:30 +0300

    test: add success pass to validateProductsGet.test.ts

--- src/router/__tests__/validateProductsGet.test.ts
+++ src/router/__tests__/validateProductsGet.test.ts
@@ -11,7 +11,7 @@ describe('validateProductsGet', () => {
   beforeEach(() => {
     jest.resetModules();
   });
-  test('when query is array but not boolean  should return invalid parameter', () => {
+  test('when query is array but not boolean should return "Invalid parameter"', () => {
     const isAvailable = [123, 321];
     const req = {
       query: {
@@ -22,5 +22,37 @@ describe('validateProductsGet', () => {
     validateProductsGet(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
     expect(sendMessage).toHaveBeenCalledWith('Invalid parameter');
+  });
+
+  test('when query is "true" should return "Invalid parameter"', () => {
+    const req = {
+      query: {
+        isAvailable: 'true'
+      }
+    };
+    // @ts-ignore for test purposes
+    validateProductsGet(req, res);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(sendMessage).not.toHaveBeenCalled();
+  });
+
+  test('when query is "false" should return "Invalid parameter"', () => {
+    const req = {
+      query: {
+        isAvailable: 'false'
+      }
+    };
+    // @ts-ignore for test purposes
+    validateProductsGet(req, res);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(sendMessage).not.toHaveBeenCalled();
+  });
+
+  test('when isAvailable parameter not passed should return nothing', () => {
+    const req = { query: {} };
+    // @ts-ignore for test purposes
+    validateProductsGet(req, res);
+    expect(res.status).not.toHaveBeenCalled();
+    expect(sendMessage).not.toHaveBeenCalled();
   });
 });
