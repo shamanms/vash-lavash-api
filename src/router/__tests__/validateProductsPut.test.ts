c43905b1f8818fab6d3c6be574ccb04f122cde0a
PochUa <po4a123@gmail.com>
2022-09-19 17:32:19 +0300

    fix validateProductsPut test

--- src/router/__tests__/validateProductsPut.test.ts
+++ src/router/__tests__/validateProductsPut.test.ts
@@ -7,11 +7,54 @@ const res = {
   }))
 };
 describe('validateProductsPut', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
   test('when products !== [] should return invalid request', () => {
     const req = {};
     // @ts-ignore for test purposes
     validateProductsPut(req, res);
     expect(res.status).toHaveBeenCalledWith(400);
     expect(sendMessage).toHaveBeenCalledWith('Invalid request');
+  });
+  test('when product !== object should return invalid product', () => {
+    const req = {
+      body: [{}]
+    };
+    // @ts-ignore for test purposes
+    validateProductsPut(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(sendMessage).toHaveBeenCalledWith('Invalid product');
+  });
+  test('when product === null should return invalid product', () => {
+    const req = {
+      body: [null]
+    };
+    // @ts-ignore for test purposes
+    validateProductsPut(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(sendMessage).toHaveBeenCalledWith('Invalid product');
+  });
+  test('when product !== [] should return invalid product', () => {
+    const req = {
+      body: [[]]
+    };
+    // @ts-ignore for test purposes
+    validateProductsPut(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(sendMessage).toHaveBeenCalledWith('Invalid product');
+  });
+  test('when product.id !== string should return invalid product', () => {
+    const req = {
+      body: [
+        {
+          id: 123
+        }
+      ]
+    };
+    // @ts-ignore for test purposes
+    validateProductsPut(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(sendMessage).toHaveBeenCalledWith('Invalid product');
   });
 });
