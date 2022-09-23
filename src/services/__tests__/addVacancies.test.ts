91786d89beb318b6289d60881e10c3f49bf3bbd4
PochUa <po4a123@gmail.com>
2022-09-23 22:18:30 +0300

    fix: unit test of method vacancies

--- src/services/__tests__/addVacancies.test.ts
+++ src/services/__tests__/addVacancies.test.ts
@@ -1,10 +1,9 @@
 import db from '../../models';
-import { VacancyModel } from '../../types';
 import services from '../index';
 
 jest.mock('../../models', () => ({
   vacancies: {
-    insertMany: jest.fn()
+    insertOne: jest.fn()
   }
 }));
 
@@ -12,21 +11,22 @@ describe('Service.addVacancies', () => {
   beforeEach(() => {
     jest.resetModules();
   });
-  test('when called with vacancies = [] call db with vacancies', async () => {
-    const vacancies: VacancyModel[] = [];
-    await services.vacancies.addVacancies(vacancies);
-    expect(db.vacancies.insertMany).toHaveBeenCalledWith(vacancies);
+  test('when called with vacancies = {} call db with vacancies', async () => {
+    const vacancy = {};
+    // @ts-ignore for test purposes
+    await services.vacancies.addVacancies(vacancy);
+    expect(db.vacancies.insertOne).toHaveBeenCalledWith(vacancy);
   });
 
   test('when called with vacancies = string call db with string', async () => {
     // @ts-ignore for test purposes
     await services.vacancies.addVacancies(['string']);
-    expect(db.vacancies.insertMany).toHaveBeenCalledWith(['string']);
+    expect(db.vacancies.insertOne).toHaveBeenCalledWith(['string']);
   });
 
   test('when called with products = null call db with null', async () => {
     // @ts-ignore for test purposes
     await services.vacancies.addVacancies([null]);
-    expect(db.vacancies.insertMany).toHaveBeenCalledWith([null]);
+    expect(db.vacancies.insertOne).toHaveBeenCalledWith([null]);
   });
 });
