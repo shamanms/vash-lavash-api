9d54da2aee0cd560e5d91eb9339287c740519919
PochUa <po4a123@gmail.com>
2022-09-26 21:56:10 +0300

    test: add unit test for validation loginPost

---
+++ src/router/auth/__tests__/validationLoginPost.test.ts
@@ -0,0 +1,102 @@
+import { ValidationError } from '../../../models/errors';
+import { loginPost } from '../validation';
+
+const next = jest.fn();
+const res = {};
+
+describe('loginPost', () => {
+  test("when req haven't body should return 'Invalid request'", async () => {
+    const req = {};
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid request');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test("when req body is not obj should return 'Invalid request'", async () => {
+    const req = {
+      body: []
+    };
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid request');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test("when req body is empty object should return 'Invalid username or password'", async () => {
+    const req = {
+      body: {}
+    };
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid username or password');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test("when body is object and don`t have key username should return 'Invalid username or password'", async () => {
+    const req = {
+      body: {
+        password: 12
+      }
+    };
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid username or password');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test("when body is object and have key username but don`t have key password should return 'Invalid username or password'", async () => {
+    const req = {
+      body: {
+        username: 'kit'
+      }
+    };
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid username or password');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test("when body have key username and password but value one more not string should return 'Invalid username or password'", async () => {
+    const req = {
+      body: {
+        username: 'kot',
+        password: 12
+      }
+    };
+    try {
+      // @ts-ignore for test purposes
+      await loginPost(req, res, next);
+    } catch (e: any) {
+      expect(e).toBeInstanceOf(ValidationError);
+      expect(e?.message).toMatch('Invalid username or password');
+      expect(next).not.toHaveBeenCalled();
+    }
+  });
+  test('when request is valid should go next', async () => {
+    const req = {
+      body: {
+        username: 'kot',
+        password: 'korm'
+      }
+    };
+    // @ts-ignore for test purposes
+    await loginPost(req, res, next);
+    expect(next).toHaveBeenCalled();
+  });
+});
