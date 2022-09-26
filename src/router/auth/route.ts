a08b3f672276a6f48f78202aafaf4e39e94c0f28
PochUa <po4a123@gmail.com>
2022-09-26 13:04:36 +0300

    feat: add get jwt secret from GCP

--- src/router/auth/route.ts
+++ src/router/auth/route.ts
@@ -1,33 +1,45 @@
 import { Middleware, TypedRequestBody } from '../../types';
 import jsonwebtoken from 'jsonwebtoken';
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
 
 export type LoginRequest = Middleware<
   TypedRequestBody<{ username: string; password: string }>
 >;
+const { PROJECT_ID, JWT_SECRET_NAME, JWT_SECRET_VERSION } = process.env;
+const name = `projects/${PROJECT_ID}/secrets/${JWT_SECRET_NAME}/versions/${JWT_SECRET_VERSION}`;
+const client = new SecretManagerServiceClient();
 
-// TODO: get from GCP secret manager
-// The secret should be an unguessable long string (you can use a password generator for this!)
-const JWT_SECRET =
-  'goK!pusp6ThEdURUtRenOwUhAsWUCLheBazl!uJLPlS8EbreWLdrupIwabRAsiBu';
+async function accessSecretVersion() {
+  const [version] = await client.accessSecretVersion({
+    name: name
+  });
+
+  return version?.payload?.data?.toString() || '';
+}
 
 export const login: LoginRequest = async (req, res, next) => {
-  const { username, password } = req.body;
-  console.log(`${username} is trying to login ..`);
+  try {
+    const jwtSecret = await accessSecretVersion();
+    const { username, password } = req.body;
+    console.log(`${username} is trying to login ..`);
 
-  // TODO: get from DB Model + Service
-  const user = {
-    username: 'admin',
-    password: 'mySecretPwd',
-    role: 'admin'
-  };
+    // TODO: get from DB Model + Service
+    const user = {
+      username: 'admin',
+      password: 'mySecretPwd',
+      role: 'admin'
+    };
 
-  if (username === user.username && password === user.password) {
-    return res.json({
-      token: jsonwebtoken.sign({ role: user.role }, JWT_SECRET)
-    });
+    if (username === user.username && password === user.password) {
+      return res.json({
+        token: jsonwebtoken.sign({ role: user.role }, jwtSecret)
+      });
+    }
+
+    return res
+      .status(401)
+      .json({ message: 'The username and password your provided are invalid' });
+  } catch (e) {
+    next(e);
   }
-
-  return res
-    .status(401)
-    .json({ message: 'The username and password your provided are invalid' });
 };
