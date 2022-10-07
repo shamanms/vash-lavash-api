6932dc9459133e68a2308c961b599546f6f2d442
PochUa <po4a123@gmail.com>
2022-10-07 14:58:13 +0300

    feat: add message for glovo order

--- src/services/orderNotificationClass.ts
+++ src/services/orderNotificationClass.ts
@@ -10,12 +10,26 @@ export class OrderNotification {
 
   private composeMessage() {
     const { API_URL } = process.env;
-    const { phone, totalPrice, items, id } = this.order;
+    const { phone, totalPrice, items, id, glovoOrderId } = this.order;
     const confirmedUrl = `${API_URL}/orders/${id}?status=${OrderStatus.CONFIRMED}`;
     const completedUrl = `${API_URL}/orders/${id}?status=${OrderStatus.COMPLETED}`;
 
-    return `
+    if (glovoOrderId === undefined) {
+      return `
         <b>НОВЕ ЗАМОВЛЕННЯ!</b>
+Tелефон: <a href="tel:+38${phone.replace('[^0-9]', '')}">${phone}</a>
+Сума: ${totalPrice}UAH
+Товари:
+${items.map((item) => `${item.name}: ${item.count}шт;`).join('\n')}
+
+
+<a href="${confirmedUrl}">ПІДТВЕРДЖЕНО</a>
+
+
+<a href="${completedUrl}">ВИДАНО</a>`;
+    } else
+      return `
+        <b>НОВЕ ЗАМОВЛЕННЯ! GLOVO!</b>
 Tелефон: <a href="tel:+38${phone.replace('[^0-9]', '')}">${phone}</a>
 Сума: ${totalPrice}UAH
 Товари:
