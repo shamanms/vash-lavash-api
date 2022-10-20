f0e207690d2c692f6eabf300e0ab0a0e42c05bf2
PochUa <po4a123@gmail.com>
2022-10-20 16:48:44 +0300

    fix: unit test

--- src/utils/__tests__/dateTimeFormatter.test.ts
+++ src/utils/__tests__/dateTimeFormatter.test.ts
@@ -2,6 +2,12 @@ import { dateTimeFormatter } from '../dateTimeFormatter';
 
 describe('utils dateTimeFormatter', () => {
   test('when date is number should return formatted date in string', () => {
-    expect(dateTimeFormatter(1645674000000)).toEqual('24/02/2022 05:40');
+    const date = '24/02/2022';
+    const time = '04:05';
+    expect(
+      dateTimeFormatter(
+        new Date(`${date.split('/').reverse().join('/')} ${time}`).getTime()
+      )
+    ).toEqual(`${date} ${time}`);
   });
 });
