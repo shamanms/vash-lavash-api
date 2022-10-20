3de177ca2f0e9938e69143d2e4269e18a586edde
PochUa <po4a123@gmail.com>
2022-10-20 14:30:42 +0300

    test: fix unit test

--- src/utils/__tests__/dateTimeFormatter.test.ts
+++ src/utils/__tests__/dateTimeFormatter.test.ts
@@ -2,6 +2,6 @@ import { dateTimeFormatter } from '../dateTimeFormatter';
 
 describe('utils dateTimeFormatter', () => {
   test('when date is number should return formatted date in string', () => {
-    expect(dateTimeFormatter(1645674000623)).toEqual('24/02/2022 05:40');
+    expect(dateTimeFormatter(1645674000625)).toEqual('24/02/2022 05:40');
   });
 });
