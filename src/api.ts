2e418f015b7d75f4141fca537d9a79cfa3b2113b
PochaUa <112777090+PochaUa@users.noreply.github.com>
2023-03-01 15:31:20 +0200

    feat: add logger for routes

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import audit from 'express-requests-logger';
 
 import router from './router';
 import { errorHandler } from './router/errorHandler';
@@ -7,10 +8,19 @@ import { errorHandler } from './router/errorHandler';
 const { ALLOWED_DOMAINS } = process.env;
 
 export const app = express();
+const logger = audit({
+  doubleAudit: true,
+  request: {
+    maskBody: ['password', 'token']
+  },
+  response: { maskBody: ['password', 'token'] }
+});
+
 app.use(
   cors({
     origin: ALLOWED_DOMAINS?.split('|')
   })
 );
+app.use(logger);
 app.use(router);
 app.use(errorHandler);
